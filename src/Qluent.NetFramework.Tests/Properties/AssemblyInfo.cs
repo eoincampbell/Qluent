fe530efc7130023f5368d40f02535eebf7026603
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-20 19:15:08 +0100

    Fixed Assembly Info for non netFW projects

--- src/Qluent.NetFramework.Tests/Properties/AssemblyInfo.cs
+++ src/Qluent.NetFramework.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,6 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Qluent.NetFramework.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Qluent.NetFramework.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
+[assembly: AssemblyDescription(".NET Framework Project containing NUnit Tests for Qluent")]
 [assembly: Guid("28729069-27ed-48a0-a496-4e493830bf76")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
