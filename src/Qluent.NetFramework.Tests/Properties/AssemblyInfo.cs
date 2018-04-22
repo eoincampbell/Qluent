2d150afea539ca312cabf66a6378c05a437e5f02
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-22 13:08:41 +0100

    Deleted the old NetFw Test PRoject

--- src/Qluent.NetFramework.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Qluent.NetFramework.Tests")]
-[assembly: AssemblyDescription(".NET Framework Project containing NUnit Tests for Qluent")]
-[assembly: Guid("28729069-27ed-48a0-a496-4e493830bf76")]
