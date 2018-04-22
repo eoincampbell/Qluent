de540ec4f15dde98f214e3526fc2241b53139b77
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-22 13:57:59 +0100

    Reverted Global Assembly. Using new package versioning instead

--- src/Qluent.NetCore.ConsumerTestHarness/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Qluent.NetCore.ConsumerTestHarness")]
-[assembly: AssemblyDescription(".NET Core Project containing Test Harness Example for Qluent Consumer")]
-[assembly: Guid("98E318D3-98D3-4EF0-98B4-C10FE962B911")]
--- src/Qluent/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Qluent")]
-[assembly: AssemblyDescription("A Fluent Queue Client for creating Azure Storage Queues and Consumers")]
-[assembly: Guid("A7E8424A-0DC9-4B7B-9C55-A2541807249E")]
