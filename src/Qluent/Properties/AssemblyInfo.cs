ba540a604c8f3e4442716b2229a06c675475b3d0
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-22 12:28:02 +0100

    Prep for building nuget package

--- src/Qluent/Properties/AssemblyInfo.cs
+++ src/Qluent/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Qluent.NetCore.ConsumerTestHarness")]
-[assembly: AssemblyDescription(".NET Core Project containing Test Harness Example for Qluent Consumer")]
+[assembly: AssemblyTitle("Qluent")]
+[assembly: AssemblyDescription("A Fluent Queue Client for creating Azure Storage Queues and Consumers")]
 [assembly: Guid("A7E8424A-0DC9-4B7B-9C55-A2541807249E")]
