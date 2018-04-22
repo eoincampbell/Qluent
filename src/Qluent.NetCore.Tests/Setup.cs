ec5d3a7b6e49f4f18fdcc402ae818e86406e7753
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-22 11:32:25 +0100

    Moving AzureStorageEmulator Startup to yml file

--- src/Qluent.NetCore.Tests/Setup.cs
+++ src/Qluent.NetCore.Tests/Setup.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NUnit.Framework;
-using Qluent.NetCore.Tests.Helper;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using NUnit.Framework;
+//using Qluent.NetCore.Tests.Helper;
 
-namespace Qluent.NetCore.Tests
-{
-    [SetUpFixture]
-    public class StartStopAzureEmulator
-    {
-        private bool _wasUp;
+//namespace Qluent.NetCore.Tests
+//{
+//    [SetUpFixture]
+//    public class StartStopAzureEmulator
+//    {
+//        private bool _wasUp;
 
-        [OneTimeSetUp]
-        public void StartAzureBeforeAllTestsIfNotUp()
-        {
-            if (!AzureStorageEmulatorManager.IsProcessStarted())
-            {
-                AzureStorageEmulatorManager.Start();
-                _wasUp = false;
-            }
-            else
-            {
-                _wasUp = true;
-            }
+//        [OneTimeSetUp]
+//        public void StartAzureBeforeAllTestsIfNotUp()
+//        {
+//            if (!AzureStorageEmulatorManager.IsProcessStarted())
+//            {
+//                AzureStorageEmulatorManager.Start();
+//                _wasUp = false;
+//            }
+//            else
+//            {
+//                _wasUp = true;
+//            }
 
-        }
+//        }
 
-        [OneTimeTearDown]
-        public void StopAzureAfterAllTestsIfWasDown()
-        {
-            if (!_wasUp)
-            {
-                AzureStorageEmulatorManager.Stop();
-            }
-        }
-    }
-}
+//        [OneTimeTearDown]
+//        public void StopAzureAfterAllTestsIfWasDown()
+//        {
+//            if (!_wasUp)
+//            {
+//                AzureStorageEmulatorManager.Stop();
+//            }
+//        }
+//    }
+//}
