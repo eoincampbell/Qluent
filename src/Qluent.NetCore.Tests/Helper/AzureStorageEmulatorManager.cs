8c6724cfd425cd27420e6a8bfef336f7729acdb6
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-22 11:12:59 +0100

    Fix to autostart the emulator on the build machine for AppVeyor

---
+++ src/Qluent.NetCore.Tests/Helper/AzureStorageEmulatorManager.cs
@@ -0,0 +1,53 @@
+﻿namespace Qluent.NetCore.Tests.Helper
+{
+    using System.Diagnostics;
+    using System.Linq;
+
+    public static class AzureStorageEmulatorManager
+    {
+        private const string WindowsAzureStorageEmulatorPath =
+            @"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe";
+
+        private const string Win10ProcessName = "AzureStorageEmulator";
+
+        private static readonly ProcessStartInfo StartStorageEmulator = new ProcessStartInfo
+        {
+            FileName = WindowsAzureStorageEmulatorPath,
+            Arguments = "start",
+        };
+
+        private static readonly ProcessStartInfo StopStorageEmulator = new ProcessStartInfo
+        {
+            FileName = WindowsAzureStorageEmulatorPath,
+            Arguments = "stop",
+        };
+
+        private static Process GetProcess()
+        {
+            return Process.GetProcessesByName(Win10ProcessName).FirstOrDefault();
+        }
+
+        public static bool IsProcessStarted()
+        {
+            return GetProcess() != null;
+        }
+
+        public static void Start()
+        {
+            if (IsProcessStarted()) return;
+
+            using (var process = Process.Start(StartStorageEmulator))
+            {
+                process?.WaitForExit();
+            }
+        }
+
+        public static void Stop()
+        {
+            using (var process = Process.Start(StopStorageEmulator))
+            {
+                process?.WaitForExit();
+            }
+        }
+    }
+}
---
+++ src/Qluent.NetCore.Tests/Setup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Qluent.NetCore.Tests.Helper;
+
+namespace Qluent.NetCore.Tests
+{
+    [SetUpFixture]
+    public class StartStopAzureEmulator
+    {
+        private bool _wasUp;
+
+        [OneTimeSetUp]
+        public void StartAzureBeforeAllTestsIfNotUp()
+        {
+            if (!AzureStorageEmulatorManager.IsProcessStarted())
+            {
+                AzureStorageEmulatorManager.Start();
+                _wasUp = false;
+            }
+            else
+            {
+                _wasUp = true;
+            }
+
+        }
+
+        [OneTimeTearDown]
+        public void StopAzureAfterAllTestsIfWasDown()
+        {
+            if (!_wasUp)
+            {
+                AzureStorageEmulatorManager.Stop();
+            }
+        }
+    }
+}
